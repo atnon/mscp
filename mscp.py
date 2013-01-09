c87b94e6514054fb8f4e4c575b1caf82bb0f2f7a
Anton Landberg <git@atnon.se>
2013-01-09 05:16:47 +0100

    Cleanup and commenting

--- mscp.py
+++ mscp.py
@@ -5,82 +5,80 @@ import subprocess as sub
 import threading
 import os
 
-con = None
-
-# The following pattern will match the different parts of the log strings.
-logPattern = re.compile("^(?P<date>\d{4}-\d{2}-\d{2})\s(?P<time>\d\d:\d\d:\d\d)\s\[(?P<level>.+?)\]\s(?P<message>.*)$", re.M)
-
 class dbWorker(threading.Thread):
-	def run(self):
-		print "Started dbWorker thread. (ID: %s)" % self.getName()
-		try:
-		        con = mdb.connect('localhost', 'mmc', 'mmc', 'mmc')
-		        cursor = con.cursor()
-		        print "Connected to db..."
-		        p = sub.Popen('tail -fn50 /opt/msm/servers/hallian/server.log', shell = True, stdout = sub.PIPE)
-		        for line in iter(p.stdout.readline, ''):
-		                r = logPattern.match(line)
-		                if r:
-		                        if re.match('INFO|WARNING|SEVERE', r.group('level')):
-		                                level = r.group('level')
-		                        else:
-		                                level = 'UNKNOWN'
-		                        cursor.execute("INSERT INTO log(date,time,level,message) VALUES (%s, %s, %s, %s)",(r.group('date'), r.group('time'), level, r.group('message')))
-		                        con.commit()
-		                print(line),
-			
-		except mdb.Error, e:
-			conn.rollback()
-			print "Error %d: %s" % (e.args[0],e.args[1])
-		finally:
-			if con:
-				con.close()
+    """Reads log and insert lines into database."""
+    def run(self):
+        print "Started dbWorker thread. (ID: %s)" % self.getName()
+        # RegEx for breaking log strings into its components.
+        logPattern = re.compile("^(?P<date>\d{4}-\d{2}-\d{2})\s" +
+                                "(?P<time>\d\d:\d\d:\d\d)\s" +
+                                "\[(?P<level>.+?)\]\s(?P<message>.*)$",
+                                re.M)
+        con = None
+        try:
+            # Connect to database
+            con = mdb.connect('localhost', 'mmc', 'mmc', 'mmc')
+            cursor = con.cursor()
+            print "Connected to db."
+            # Run subprocess reading log.
+            p = sub.Popen(
+                    'tail -fn50 /opt/msm/servers/hallian/server.log',
+                    shell = True, stdout = sub.PIPE)
+            for line in iter(p.stdout.readline, ''):
+                # Break logline into parts.
+                r = logPattern.match(line)
+                # Handle loglevels.
+                if r:
+                    if re.match('INFO|WARNING|SEVERE', r.group('level')):
+                        level = r.group('level')
+                    else:
+                        level = 'UNKNOWN'
+                    # Prepare db insertion and commit.
+                    cursor.execute(
+                        ''.join(["INSERT INTO log(date,time,level,message)",
+                        "VALUES (%s, %s, %s, %s)"]),(r.group('date'),
+                        r.group('time'), level, r.group('message')))
+                    con.commit()
+                # Output line to stdout
+                print(line),
+
+        except mdb.Error, e:
+            # Do some exception handling concerning the database.
+            conn.rollback()
+            print "Error %d: %s" % (e.args[0],e.args[1])
+        finally:
+            if con:
+                con.close()
 
 class cmdListner(threading.Thread):
-	def run(self):
-		self.pipePath = '/var/www/piper'
-		try:
-			os.unlink(self.pipePath)
-		except:
-			pass
-		os.mkfifo(self.pipePath)
-		pipe = open(self.pipePath,'rw')
-		while True:
-			data = pipe.readline()
-			if data != '':
-		        	msm = sub.Popen(['msm', 'hallian', 'cmd', data], shell = False, stdout = sub.PIPE)
-				
-			
-				
-				
-db = dbWorker()
-msm = cmdListner()
-db.start()
-msm.start()
+    """Listens for and issues commands to Minecraft server."""
+    def run(self):
+        print "Started cmdListner thread. (ID: %s)" % self.getName()
+        # Path to pipe.
+        self.pipePath = '/var/www/piper'
+        # Attempt to remove pipe first, since  trying to create an already 
+        # existing pipe will result in an error.
+        try:
+            os.unlink(self.pipePath)
+        except:
+            pass
+        # Create pipe.
+        os.mkfifo(self.pipePath)
+        # Make pipe writeable.
+        os.chmod(self.pipePath, 0666)
+        # Open pipe for reading.
+        pipe = open(self.pipePath,'r')
+        while True:
+            data = pipe.readline()
+            if data != '':
+                # If a command has been issued, send it to server.
+                msm = sub.Popen(['msm', 'hallian', 'cmd', data],
+                                shell = False, stdout = sub.PIPE)
 
-
-
-"""try:
-	con = mdb.connect('localhost', 'mmc', 'mmc', 'mmc')
-	cursor = con.cursor()
-	print "Connected to db..."
-	p = sub.Popen('tail -fn50 /opt/msm/servers/hallian/server.log', shell = True, stdout = sub.PIPE)
-	for line in iter(p.stdout.readline, ''):
-		r = logPattern.match(line)
-		if r:
-			if re.match('INFO|WARNING|SEVERE', r.group('level')):
-				level = r.group('level')
-			else:
-				level = 'UNKNOWN'
-			cursor.execute("INSERT INTO log(date,time,level,message) VALUES (%s, %s, %s, %s)",(r.group('date'), r.group('time'), level, r.group('message')))
-			con.commit()
-		print(line),
-except mdb.Error, e:
-	conn.rollback()
-	print "Error %d: %s" % (e.args[0],e.args[1])
-	sys.exit(1)
-
-finally:
-	if con:
-		con.close()
-"""
+if __name__ == "__main__":
+    # Setup workers.
+    db = dbWorker()
+    msm = cmdListner()
+    # Start workers.
+    db.start()
+    msm.start()
