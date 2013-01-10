fec0f2ec7057b6af6f6d0ee7fc59d0d5ef064fac
Anton Landberg <git@atnon.se>
2013-01-10 20:05:12 +0100

    Added main, check for keyboardInterrupt and exit methods. Also defined class createPipe outside worker thread.

--- mscp.py
+++ mscp.py
@@ -1,9 +1,12 @@
-import fileinput
 import MySQLdb as mdb
 import re
 import subprocess as sub
 import threading
 import os
+import signal
+import sys
+
+threadExit = False
 
 class dbWorker(threading.Thread):
     """Reads log and insert lines into database."""
@@ -40,7 +43,9 @@ class dbWorker(threading.Thread):
                         r.group('time'), level, r.group('message')))
                     con.commit()
                 # Output line to stdout
-                print(line),
+                #print(line),
+                if threadExit:
+                    break
 
         except mdb.Error, e:
             # Do some exception handling concerning the database.
@@ -49,36 +54,68 @@ class dbWorker(threading.Thread):
         finally:
             if con:
                 con.close()
+        #
+        print "%s: Exit" % self.getName()
 
 class cmdListner(threading.Thread):
     """Listens for and issues commands to Minecraft server."""
-    def run(self):
-        print "Started cmdListner thread. (ID: %s)" % self.getName()
+    def __init__(self):
         # Path to pipe.
         self.pipePath = '/var/www/piper'
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print "Started cmdListner thread. (ID: %s)" % self.getName()
+        # Open pipe for reading.
+        with createPipe(self.pipePath) as pipe:
+            while not threadExit:
+                data = pipe.readline()
+                if data != '':
+                    # If a command has been issued, send it to server.
+                    msm = sub.Popen(['msm', 'hallian', 'cmd', data],
+                                shell = False, stdout = sub.PIPE)
+
+class createPipe:
+    """ Initializes a pipe and opens it for listening
+        Also does cleaning when done.
+    """
+    def __init__(self, pipePath):
+        self.pipePath = pipePath
+
+    def __enter__(self):
         # Attempt to remove pipe first, since  trying to create an already 
         # existing pipe will result in an error.
         try:
             os.unlink(self.pipePath)
         except:
             pass
+
         # Create pipe.
         os.mkfifo(self.pipePath)
         # Make pipe writeable.
         os.chmod(self.pipePath, 0666)
-        # Open pipe for reading.
-        pipe = open(self.pipePath,'r')
-        while True:
-            data = pipe.readline()
-            if data != '':
-                # If a command has been issued, send it to server.
-                msm = sub.Popen(['msm', 'hallian', 'cmd', data],
-                                shell = False, stdout = sub.PIPE)
+        return open(self.pipePath, 'r')
+    
+    def __exit__(self, type, value, traceback):
+        # Clean up the pipe we created.
+        os.unlink(self.pipePath)
 
-if __name__ == "__main__":
+def main():
     # Setup workers.
     db = dbWorker()
     msm = cmdListner()
     # Start workers.
     db.start()
     msm.start()
+
+
+if __name__ == "__main__":
+    # Setup handler for KeyboardInterrupt (Ctrl+C)
+    def keyInt(signal, frame):
+        print 'Exiting...'
+        treadExit = True
+        sys.exit()
+    signal.signal(signal.SIGINT, keyInt)
+
+    #Execute main program
+    main()
